1cad88616472444c76e96a04159fa4ed7e37052e
Camilo Morales <camiloarturomoralesb@gmail.com>
2024-08-15 09:56:44 -0500

    correccion de la revision

--- app.py
+++ app.py
@@ -10,18 +10,31 @@ def load_data():
 
 car_data = load_data()
 
-
+# Crear el contenido de la aplicación
 st.header('Explorador de Datos de Vehículos')
 
-
+# Botón para construir el histograma
 hist_button = st.button('Construir histograma')
 
 if hist_button:
-    # Escribir un mensaje
+    # Escribir un mensaje en la interfaz
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # Crear un histograma con Plotly Express
-    fig = px.histogram(car_data, x="odometer", title="Histograma de Odometer")
+    fig_hist = px.histogram(car_data, x="odometer", title="Histograma de Odometer")
 
-    # Mostrar el gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    # Mostrar el gráfico Plotly interactivo en la aplicación
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Botón para construir el gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    # Escribir un mensaje en la interfaz
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un gráfico de dispersión con Plotly Express
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Gráfico de Dispersión: Odometer vs Price")
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación
+    st.plotly_chart(fig_scatter, use_container_width=True)
